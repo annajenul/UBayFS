090e47327d418dbd9528a1d24a00fc2e137022a2
Anna Jenul <anna.jenul@gmail.com>
2022-11-10 22:22:16 +0200

    update

--- R/runInteractive.R
+++ R/runInteractive.R
@@ -4,6 +4,11 @@
 #' @export
 
 runInteractive = function(){
+
+  if(!require("caret")){stop("For this application, the caret package is required.")}
+  if(!require("GSelection")){stop("For this application, the GSelection package is required.")}
+  if(!require("glmnet")){stop("For this application, the glmnet package is required.")}
+  if(!require("rpart")){stop("For this application, the rpart package is required.")}
   appDir <- system.file("shinyApp", "UBayFSInteractive", package = "UBayFS")
   if (appDir == "") {
     stop("Could not find app directory. Try re-installing `UBayFS`.", call. = FALSE)
